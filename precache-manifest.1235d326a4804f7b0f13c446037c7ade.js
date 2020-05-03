0a223ef8c3fd29a283fd1ebd0ea527371815e94e
wilk6 <wilk6996@gmail.com>
2020-05-03 13:31:28 +0200

    Updates

---
+++ precache-manifest.1235d326a4804f7b0f13c446037c7ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1cccfe4f94631cc00d519a272aed98",
+    "url": "/project00/index.html"
+  },
+  {
+    "revision": "842e7859a8436a0a1de4",
+    "url": "/project00/static/css/main.1e8fcc16.chunk.css"
+  },
+  {
+    "revision": "10b7513e99d956d874b5",
+    "url": "/project00/static/js/2.963abef8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project00/static/js/2.963abef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842e7859a8436a0a1de4",
+    "url": "/project00/static/js/main.1e584855.chunk.js"
+  },
+  {
+    "revision": "773974823ebe6f63698a",
+    "url": "/project00/static/js/runtime-main.8b47bf6d.js"
+  },
+  {
+    "revision": "c7fdb3ed6f5d1b76c7defaae70bda2b3",
+    "url": "/project00/static/media/Allura-Regular.c7fdb3ed.otf"
+  }
+]);
